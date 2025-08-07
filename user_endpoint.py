e591a1a4148d7111c6d1050cf10859d43d8306e1
J.A.R.V.I.K <jiri.cechura@gmail.com>
2025-08-07 07:05:42 +0200

    Create user_endpoint.py

---
+++ user_endpoint.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter, Request
+from user_utils import get_user_from_token
+
+router = APIRouter()
+
+@router.get("/user")
+async def get_user(request: Request):
+    user = get_user_from_token(request)
+    if not user:
+        return {"error": "Unauthorized"}
+    return user
