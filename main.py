096bc11046bc38d7c189137814eeb89583da7fe8
J.A.R.V.I.K <jiri.cechura@gmail.com>
2025-08-07 07:08:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 from api.get_context import router as context_router
 from api.auth import router as auth_router
 from api.crawler_router import router as crawler_router
-from middleware import APIKeyMiddleware  # Přidáno
+from user_endpoint import router as user_router  # ⬅️ Přidat
+from middleware import APIKeyMiddleware
 
 app = FastAPI(title="Otec Fura")
 
@@ -13,6 +14,7 @@ app.add_middleware(APIKeyMiddleware)
 app.include_router(context_router)
 app.include_router(auth_router)
 app.include_router(crawler_router)
+app.include_router(user_router)  # ⬅️ Přidat
 
 @app.get("/")
 async def root():
