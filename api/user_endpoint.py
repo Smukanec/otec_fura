2e74493765540209e4d16a001918e0e2dfb28513
J.A.R.V.I.K <jiri.cechura@gmail.com>
2025-08-07 08:11:37 +0200

    Update user_endpoint.py

--- api/user_endpoint.py
+++ api/user_endpoint.py
@@ -1,17 +1,11 @@
 from fastapi import APIRouter, Request
-import sys
-import os
-
-sys.path.append(os.path.abspath("scripts"))
-
-from user_utils import get_user_from_token
+from scripts.user_utils import get_user_from_token
 
 router = APIRouter()
 
 @router.get("/user")
 async def get_user(request: Request):
     user = get_user_from_token(request)
-    return {
-        "username": user["username"],
-        "model": user.get("model", "nezadán")
-    }
+    if not user:
+        return {"error": "Unauthorized"}
+    return user
