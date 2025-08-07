72fdbade99f95852b5e6365af63b3920f792c559
J.A.R.V.I.K <jiri.cechura@gmail.com>
2025-08-07 22:21:21 +0200

    Update get_context.py

--- api/get_context.py
+++ api/get_context.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter, Request
-from api.get_memory import append_to_memory
+from api.get_memory import load_memory_context, append_to_memory  # ← přidat i load_memory_context
 from api.search_knowledge import search_knowledge
 from api.embedder import embed_and_query
 
 router = APIRouter()
-
 
 @router.post("/get_context")
 async def get_context(request: Request):
@@ -15,6 +14,9 @@ async def get_context(request: Request):
     memory_ctx = load_memory_context(user, query)
     knowledge_ctx = search_knowledge(query)
     embed_ctx = embed_and_query(query)
+
+    # 🔸 Volitelný zápis do paměti
+    append_to_memory(user, query, "Zaznamenán dotaz přes /get_context.")
 
     return {
         "memory": memory_ctx,
